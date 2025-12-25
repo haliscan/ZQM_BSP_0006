88e8f76d3b4c8c55ec999a46fdf44d710f84a892
haliscan <can.halis@outlook.com>
2025-12-25 11:56:23 +0300

    feat: Enhance Report Controller with User Information Handling
    - Integrated Shell Container to retrieve logged-in user details.
    - Updated onInit method to set model size limit and fetch user information.
    - Refactored _getUserInformation to handle user email verification via BTP Workzone.
    - Added onInitFilter method to configure supplier filter based on user role.
    - Implemented error handling for user access validation.
    - Introduced utility methods for reading user data and managing view model.
    - Updated deployment configuration with new transport request.

--- dist/controller/Report-dbg.controller.js
+++ dist/controller/Report-dbg.controller.js
@@ -4,62 +4,110 @@ sap.ui.define(
         "../model/formatter",
         "sap/m/MessageBox",
         "sap/ui/core/BusyIndicator",
+        "sap/ushell/Container",
     ],
     (
         Controller,
         formatter,
         MessageBox,
-        BusyIndicator
+        BusyIndicator,
+        ShellCont,
+
     ) => {
+
         "use strict";
+
+        var oData = {};
 
         return Controller.extend("com.karsan.qm.taskmanage.controller.Report", {
             formatter: formatter,
             onInit: function () {
+                oData = this.getOwnerComponent().getModel();
+                oData.setSizeLimit(1000000);
                 this.getRouter().getRoute("Report").attachMatched(this._onRouteMatched, this);
             },
 
             _onRouteMatched: function () {
-                this.getOwnerComponent().getModel().metadataLoaded().then(() => {
-                    this._getUserInformation();
+                this.getOwnerComponent().getModel().metadataLoaded().then(async () => {
+                    await this._getUserInformation();
+
+                    let sLoggedInUser = ShellCont.getUser().getId().toUpperCase();
+                    const sUserPath = oData.createKey("/UserSet", { Uname: sLoggedInUser });
+                    const oUser = await this.onRead(sUserPath, oData);
+                    this.getViewModel().setProperty("/LoggedUser", oUser);
+
+                    await this.onInitFilter();
+                    this._setSFBconfig();
                 });
-                this._setSFBconfig();
+
             },
 
-            _getUserInformation: function () {
-                if (window.top === window) return;
+            onInitFilter: async function () {
 
-                BusyIndicator.show();
-                this._readBtpWorkzoneId().then((oResult) => {
-                    const sBTPWorkzoneID = oResult.BtpWorkzoneID || oResult.BtpLinkID || oResult.BtpLink || "";
-                    const fnHandleMessage = async (oEvent) => {
-                        if (oEvent.origin !== sBTPWorkzoneID) return;
-                        try {
-                            const oData = JSON.parse(oEvent.data);
-                            if (oData?.RequestID === "UserMailRequest" && oData.UserMail) {
-                                clearInterval(oIntervalID);
-                                window.removeEventListener("message", fnHandleMessage);
-                                await this._verifySupplierEmail(oData.UserMail);
-                            }
-                        } catch (err) {
-                            this._rejectInvalidUserAccess(fnHandleMessage, oIntervalID);
-                        }
-                    };
+                let oUser = this.getViewModel().getProperty("/LoggedUser"); 
+                let oFbLifnr = this.byId("idSfbReportSet").getControlByKey("Lifnum");
+                if (oUser.IsSupplier) {
+                    oFbLifnr.removeAllTokens();
+                    oFbLifnr.setValue(oUser.Uname);
+                    oFbLifnr.setEnabled(false);
+                }
 
-                    window.addEventListener("message", fnHandleMessage);
-                    const oIntervalID = setInterval(() => {
-                        window.top.postMessage(
-                            JSON.stringify({
-                                RequestID: "UserMailRequest"
-                            }),
-                            sBTPWorkzoneID
-                        );
-                    }, 3000);
-                }).catch(() => {
-                    BusyIndicator.hide();
-                    this._rejectInvalidUserAccess();
+            },
+
+            // BTP Workzone User Info
+            _getUserInformation: async function () {
+                return new Promise((resolve, reject) => {
+                    if (window.top === window) {
+                        resolve(); // Standalone mode ise devam et
+                        return;
+                    }
+
+                    BusyIndicator.show();
+
+                    this._readBtpWorkzoneId()
+                        .then((oResult) => {
+                            const sBTPWorkzoneID = oResult.BtpWorkzoneID || oResult.BtpLinkID || oResult.BtpLink || "";
+
+                            let oIntervalID;
+                            const fnHandleMessage = async (oEvent) => {
+                                if (oEvent.origin !== sBTPWorkzoneID) return;
+
+                                try {
+                                    const oData = JSON.parse(oEvent.data);
+                                    if (oData?.RequestID === "UserMailRequest" && oData.UserMail) {
+                                        clearInterval(oIntervalID);
+                                        window.removeEventListener("message", fnHandleMessage);
+
+                                        await this._verifySupplierEmail(oData.UserMail);
+                                        BusyIndicator.hide();
+                                        resolve(); // ✅ Suc 
+                                    }
+                                } catch (err) {
+                                    clearInterval(oIntervalID);
+                                    window.removeEventListener("message", fnHandleMessage);
+                                    this._rejectInvalidUserAccess(fnHandleMessage, oIntervalID);
+                                    reject(err); // ✅ Error
+                                }
+                            };
+
+                            window.addEventListener("message", fnHandleMessage);
+                            oIntervalID = setInterval(() => {
+                                window.top.postMessage(
+                                    JSON.stringify({
+                                        RequestID: "UserMailRequest"
+                                    }),
+                                    sBTPWorkzoneID
+                                );
+                            }, 3000);
+                        })
+                        .catch((error) => {
+                            BusyIndicator.hide();
+                            this._rejectInvalidUserAccess();
+                            reject(error); // ✅ Error
+                        });
                 });
             },
+
             _readBtpWorkzoneId: function (sEmail) {
                 const oModel = this.getOwnerComponent().getModel("btpService");
                 return new Promise((resolve, reject) => {
@@ -69,20 +117,22 @@ sap.ui.define(
                     });
                 });
             },
+
             _verifySupplierEmail: async function (sEmail) {
                 try {
                     const oUserData = await this._readUserByEmail(sEmail);
                     this.getView().getModel().setHeaders({
                         email: sEmail
                     });
-                    BusyIndicator.hide();
+                    return oUserData; // ✅ Promise resolve
                 } catch (err) {
                     this._rejectInvalidUserAccess();
+                    throw err; // ✅ Promise reject
                 }
             },
+
             _readUserByEmail: function (sEmail) {
                 const oModel = this.getOwnerComponent().getModel("btpService");
-
                 return new Promise((resolve, reject) => {
                     oModel.read(`/SupplierLogonInfoSet('${sEmail}')`, {
                         success: resolve,
@@ -90,6 +140,7 @@ sap.ui.define(
                     });
                 });
             },
+
             _rejectInvalidUserAccess: function (fnHandleMessage, oIntervalID) {
                 BusyIndicator.hide();
                 if (oIntervalID) clearInterval(oIntervalID);
@@ -101,6 +152,20 @@ sap.ui.define(
                     onClose: () => {
                         history.go(-1);
                     }
+                });
+            },
+
+            getViewModel: function () {
+                return this.getView().getModel("viewModel");
+            },
+
+            onRead(sSet, oModel) {
+                return new Promise((fnSuccess, fnReject) => {
+                    const mParameters = {
+                        success: fnSuccess,
+                        error: fnReject
+                    };
+                    oModel.read(sSet, mParameters);
                 });
             },
 
